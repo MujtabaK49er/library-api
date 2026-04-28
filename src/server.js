1a050497fa760ceae9b268c3bc83e93385d136bb
Mujtaba <mujtaba6159@gmail.com>
2026-04-28 13:31:18 -0400

    Add Swagger API documentation

--- src/server.js
+++ src/server.js
@@ -5,12 +5,17 @@ import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
 import loanRoutes from './routes/loanRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
-
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
 const app = express();
+const swaggerDocument = yaml.load(fs.readFileSync('./docs/openapi.yaml', 'utf8'));
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/api/books', bookRoutes);
 app.use('/api/loans', loanRoutes);
 app.use('/api/reviews', reviewRoutes);
